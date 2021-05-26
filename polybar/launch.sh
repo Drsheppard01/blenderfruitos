952f6341b7667742bd0d06221b73208b22e34e00
georgybog <60893791+georgybog@users.noreply.github.com>
2021-05-26 07:36:54 +0300

    Add files via upload

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -9,5 +9,4 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch the bar
 polybar -q main -c ~/.config/polybar/config.ini &
 polybar -q spotifybar -c ~/.config/polybar/spotify.ini &
-polybar -q newsbar -c ~/.config/polybar/news.ini 
 echo "Bars launched...."
