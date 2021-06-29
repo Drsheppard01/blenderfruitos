1970640372767885954c1c45c6bfacc07a5e65ab
georgybog <60893791+georgybog@users.noreply.github.com>
2021-06-30 02:03:11 +0300

    Update home-script.sh

--- scripts/homie/home-script.sh
+++ scripts/homie/home-script.sh
@@ -23,8 +23,11 @@ echo 'Установка Wi-Fi драйвера'
 
 git clone https://github.com/gordboy/rtl8812au-5.9.3.2.git
 cd rtl8812au-5.9.3.2
-make
+echo "Драйвер собирается, пожалуйста подождите"
+make 
+echo "Драйвер собран. Сейчас будет произведена установка"
 sudo make install
+echo "Драйвер установлен. Сейчас пытаюсь его загрузить из системы"
 sudo modprobe 8812au
 
 ### Delete pre-installed apps ###
