2c6ebb438684dfda30856776cade05b749a18090
georgybog <60893791+georgybog@users.noreply.github.com>
2021-07-05 02:29:30 +0300

    Update wireless-updater-offline.sh

--- scripts/homie/wireless-updater-offline.sh
+++ scripts/homie/wireless-updater-offline.sh
@@ -3,7 +3,7 @@
 #  Name scipt: wireless-updater-offline 
 #  Release date: 2021-06-24
 #  Version: 1.0
-#  Copyright (C) Georgy Boganov | georg.b2014@outlook.com
+#  Copyright (C) Georgii Bogdanov | GBOG@protonmail.com
 
 cd rtl8812au-5.9.3.2
 make clean
