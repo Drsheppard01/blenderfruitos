1391705e79fcf3438da0c58ee6781259b331d373
DrSheppard <60893791+Drsheppard01@users.noreply.github.com>
2021-10-20 19:55:49 +0300

    Update Fast-cleaner.sh

--- scripts/homie/Fast-cleaner.sh
+++ scripts/homie/Fast-cleaner.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
 #  Name scipt: FastCleaner 
-#  Release date: 2021-07-12 
-#  Version: 1.0
+#  Release date: 2021-10-20 
+#  Version: 1.6
 #  Copyright (C) Georgii Bogdanov | GBOG@protonmail.com
 
 set -eu
 
+snap refresh
+
 LANG=C snap list --all | awk '/disabled/{print $1, $3}' |
     while read snapname revision; do
-        snap remove "$snapname" --revision="$revision"
+        sudo snap remove "$snapname" --revision="$revision"
     done
+sudo flatpak update
 sudo flatpak remove --unused
 sudo rm /var/lib/snapd/cache/*
 sudo eopkg dc
 sudo eopkg rmo
-rm -f ~/.cache/thumbnails/*
-sudo journalctl --vacuum-time=2day
+sudo journalctl --vacuum-size=64M
+sudo journalctl --vacuum-time=1days
+sudo rm -r ~/.cache/thumbnails/*
+sudo usysconf run -f
