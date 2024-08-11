0cce1c7c67e0cd15d737771dda8010e59216c74c
DrSheppard <60893791+Drsheppard01@users.noreply.github.com>
2024-08-11 14:10:34 +0300

    clean

--- scripts/feh_bing_wallpaper.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-# locale|grep -Ru '(?<=LANG=)[^.]*'\
-
-# set wallpaper with bing's daily picture using system's locale
-locale|grep LANG|awk -F'[=.]' '{print $2}' \
-  |xargs -I {} curl -L -s "https://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt={}" \
-  |xmllint --xpath "string(//url)" - \
-  |xargs -I {} feh --no-fehbg --bg-scale https://www.bing.com{}
