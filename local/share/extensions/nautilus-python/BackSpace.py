72e485c8aeac593f7813daced5ebd7489c49180d
DrSheppard <60893791+Drsheppard01@users.noreply.github.com>
2025-02-27 12:51:31 +0300

    uptowork

--- local/share/extensions/nautilus-python/BackSpace.py
+++ local/share/extensions/nautilus-python/BackSpace.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # created by linuxitos
+
+# slot.up instead win.up : https://discourse.gnome.org/t/backspace-files-in-gnome-47-fedora-41/23426
+
 import gi
 
 gi.require_version('Nautilus', '4.0')
@@ -9,7 +12,7 @@ from gi.repository import GObject, Nautilus, Gtk, GLib
 
 def idle_callback(*args):
     app = Gtk.Application.get_default()
-    app.set_accels_for_action("win.up", ["BackSpace"])
+    app.set_accels_for_action("slot.up", ["BackSpace"])
     return False
 
 
@@ -19,5 +22,5 @@ def window_added(*args):
 
 class BackspaceBack(GObject.GObject, Nautilus.ColumnProvider):
         app = Gtk.Application.get_default()
-        app.set_accels_for_action("win.up", ["BackSpace"])
+        app.set_accels_for_action("slot.up", ["BackSpace"])
         app.connect("window-added", window_added)
